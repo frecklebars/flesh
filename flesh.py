81e680fab265cda141a8e147121c4aa3d0bf2ac6
alex <popaalex440@gmail.com>
2021-12-22 21:23:00 +0000

    piss

--- flesh.py
+++ flesh.py
@@ -1,24 +1,51 @@
 import requests
+import argparse
 
-with open("key", "r") as f:
-    key = f.read()
+parser = argparse.ArgumentParser(description="FLickr turnEd Static Html cli")
+parser.add_argument("user", metavar="USER_ID")
+parser.add_argument("-a", "--album", dest="album")
+parser.add_argument("-k", "--key", dest="key")
+args = parser.parse_args()
 
-# params
-user_id = ""
+user_id = args.user
+album_id = args.album
+key = args.key
+
+if key is None:
+    with open("./key", "r") as f:
+        key = f.read()
+print("key found")
+
 template = "<a href='{photo_page}'><img src='{photo_preview}'></a>\n"
 
 # calls
-get_user_public_photos_call = "https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key={key}&user_id={id}&format=json&nojsoncallback=1"
-get_user_public_photos_call = get_user_public_photos_call.format(key=key, id=user_id)
+if album_id is not None:
+    flickr_call = "https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key={key}&photoset_id={album_id}&user_id={user_id}&format=json&nojsoncallback=1"
+    flickr_call = flickr_call.format(key=key, user_id=user_id, album_id=album_id)
+else:
+    flickr_call = "https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key={key}&user_id={user_id}&format=json&nojsoncallback=1"
+    flickr_call = flickr_call.format(key=key, user_id=user_id)
+
+print("constructed call")
 
 # links
 photo_preview_link_template = "https://live.staticflickr.com/{server_id}/{photo_id}_{secret}.jpg"
-photo_page_link_template = "https://www.flickr.com/photos/{user_id}/{photo_id}"
+photo_webpage_link_template = "https://www.flickr.com/photos/{user_id}/{photo_id}"
+
+if album_id is not None:
+    photo_webpage_link_template = photo_webpage_link_template + "in/album-{album_id}/".format(album_id=album_id)
 
 # main
-response = requests.get(get_user_public_photos_call).json()
-photos = response["photos"]["photo"]
+print("making request to: " + flickr_call)
+response = requests.get(flickr_call).json()
+print("request successful")
 
+if album_id is not None:
+    photos = response["photoset"]["photo"]
+else:
+    photos = response["photos"]["photo"]
+
+print("photos found, building gallery")
 gallery = ""
 for photo in photos:
     photo_id = photo["id"]
@@ -26,7 +53,7 @@ for photo in photos:
     secret = photo["secret"]
 
     photo_preview_link = photo_preview_link_template.format(server_id=server, photo_id=photo_id, secret=secret)
-    photo_page_link = photo_page_link_template.format(user_id=user_id, photo_id=photo_id)
+    photo_page_link = photo_webpage_link_template.format(user_id=user_id, photo_id=photo_id)
 
     photo_entry = template.format(
         photo_page = photo_page_link,
@@ -35,5 +62,6 @@ for photo in photos:
 
     gallery += photo_entry
 
+print("dumping gallery")
 with open("gallery.html", "w") as f:
     f.write(gallery)
